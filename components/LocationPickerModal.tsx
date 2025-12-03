42a6da4b1f4e5b5f45954442a3a0dab1749e18aa
eugene bosuku <bosukuege@gmail.com>
2025-12-03 14:41:54 +0100

    trying fix map crash bug on prod

--- components/LocationPickerModal.tsx
+++ components/LocationPickerModal.tsx
@@ -17,8 +17,8 @@ import {
 
 // Initialize Mapbox with access token from config
 const mapboxToken = 
-  Constants.expoConfig?.extra?.MAPBOX_TOKEN ||
-  process.env.MAPBOX_TOKEN;
+  Constants.expoConfig?.extra?.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN ||
+  process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN;
 if (mapboxToken) {
   Mapbox.setAccessToken(mapboxToken);
 }
